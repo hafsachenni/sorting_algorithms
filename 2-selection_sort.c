834acbdf97abe9828e5dc3a750fbdb09c0357f53
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-17 17:35:21 +0100

    ignore

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * swap - is a swiping bewteen @a and @b
+ * swap - is a swiping bewteen
  * @a: take a aguement and changing it with b
  * @b: take a aguement and changing it with a
  */
@@ -18,11 +18,13 @@ void swap(int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	/*looping throght the arr and check is i < than min */
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		size_t min = i;
 
-		for (size_t j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 			{
@@ -30,8 +32,8 @@ void selection_sort(int *array, size_t size)
 			}
 		/* swap the smallest elem with first elem of the sorted part*/
 		swap(&array[i], &array[min]);
-			for (size_t k = 0; k < size; k++)
-			print_array(array[k], size)
+			for (k = 0; k < size; k++)
+				print_array(array[k], size);
 		}
 
 	}
