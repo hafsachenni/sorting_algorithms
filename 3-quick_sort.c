9a3ed3182e9f48d1e09f683d99bbc48343cd9bf6
hafsachenni <hafsachenni77@gmail.com>
2023-11-18 09:32:35 -0800

    quick

---
+++ 3-quick_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * swap - is a swiping bewteen @a and @b
+ * @a: take a aguement and changing it with b
+ * @b: take a aguement and changing it with a
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+/**
+ * partition - function that partitions the array
+ * @array: array to be sorted
+ * @start: first index of array
+ * @end: last index of array
+ */
+
+int partition(int *array, int start, int end, size_t size)
+{
+	int i, j;
+	pivot = array[end];
+
+	for(i = start - 1, j = start; j < end - 1; j++)
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[j], &array[i]);
+		}
+	swap(&array[i], &array[end]);
+	print_array(array, size);
+	return (i);
+}
+
+
+
+/**
+ * quick_sort - sorts an array of ints
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void quicksort(int *array, size_t size, int end, int start)
+{
+	int a;
+	if (start < end)
+	{
+		a = partition(array, start, end);
+		quicksort(array, start, pivot - 1);
+		quicksort(array, pivot + 1, end);
+	}
+}
+
+
+
+
+void quick_sort(int *array, size_t size)
+{
+	quicksort(array, size, 0, size - 1);
+}
