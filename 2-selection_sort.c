31207f763d3c06f8ad8a9df1612ccad3db859b4f
hafsachenni <hafsachenni77@gmail.com>
2023-11-18 07:15:52 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - is a swiping bewteen @a and @b
+ * @a: take a aguement and changing it with b
+ * @b: take a aguement and changing it with a
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+/**
  * selection_sort - function that sorts a list using selection algo
  * @array: the array to be sorted
  * @size: size of the array
@@ -9,23 +23,23 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
-	int temp;
 
-	/*looping throght the arr and check is i < than min */
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
+			{
 				min = j;
+			}
 		}
-		if (min != i)
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
-		}
+
+		/* swap the smallest elem with first elem of the sorted part*/
+		swap(&array[i], &array[min]);
+		/* print the sorted array */
+		print_array(array, size);
 	}
+
 }
